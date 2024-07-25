c212c79c4d867231438657bc65be61b4f262447f
Steven Morales <fallasmoraless@gmail.com>
2024-07-25 00:00:45 -0600

    Improve Platform Menu Design Implement Hook Fetch Data

--- miplatform/src/App.js
+++ miplatform/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/auth/Login';
+import Login from './pages/auth/Login.jsx'
 import UserProfile from './pages/userProfile/UserProfile.jsx';
 import NavBar from './components/navBar/NavBar.jsx';
 import PlatformMenu from './pages/platform/platformMenu/PlatformMenu.jsx';
--- miplatform/src/pages/platform/platformMenu/PlatformMenu.jsx
+++ miplatform/src/pages/platform/platformMenu/PlatformMenu.jsx
@@ -1,16 +1,19 @@
+import '../../../App.css';
 import "./PlatformMenu.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserGraduate, faChalkboardTeacher, faUsers, faCalendarAlt, faClipboard, faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import RequireAuth from "../../../components/RequireAuth";
 
 function PlatformMenu() {
     const items = [
-        { id: 1, label: "Estudiantes", path: "/platform/students" },
-        { id: 5, label: "Profesores", path: "/platform/teachers" },
-        { id: 2, label: "Grupos", path: "/platform/groups" },
-        { id: 3, label: "Sesiones", path: "/platform/session" },
-        { id: 4, label: "Calificaciones", path: "/platform/grades" },
-        { id: 5, label: "Reportar", path: "/platform/report" },
+        { id: 1, label: "Estudiantes", path: "/platform/students", icon: faUserGraduate },
+        { id: 2, label: "Profesores", path: "/platform/teachers", icon: faChalkboardTeacher },
+        { id: 3, label: "Grupos", path: "/platform/groups", icon: faUsers },
+        { id: 4, label: "Sesiones", path: "/platform/session", icon: faCalendarAlt },
+        { id: 5, label: "Calificaciones", path: "/platform/grades", icon: faClipboard },
+        { id: 6, label: "Reportar", path: "/platform/report", icon: faFileAlt },
     ];
 
     return (
@@ -18,7 +21,8 @@ function PlatformMenu() {
             <div className="platform-container">
                 {items.map((item) => (
                     <Link key={item.id} to={item.path} className="platform-item">
-                        {item.label}
+                        <FontAwesomeIcon icon={item.icon} className="platform-icon" />
+                        <span>{item.label}</span>
                     </Link>
                 ))}
             </div>
